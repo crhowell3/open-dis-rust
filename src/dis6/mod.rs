178ac914eaf1afd47ca3489036b9cbd40e35f777
Cameron Howell <crhowell3@gmail.com>
2023-10-09 02:14:37 +0000

    Formatted mod file

--- src/dis6/mod.rs
+++ src/dis6/mod.rs
@@ -1,13 +1,13 @@
-//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation 
+//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation
 //                     (DIS) application protocol v6 and v7
 //     Copyright (C) 2023 Cameron Howell
 //
 //     Licensed under the BSD 2-Clause License
 
 pub mod common;
+pub mod distributed_emission_regeneration;
 pub mod entity_information;
-pub mod warfare;
 pub mod logistics;
 pub mod radio_communications;
 pub mod simulation_management;
-pub mod distributed_emission_regeneration;
+pub mod warfare;
