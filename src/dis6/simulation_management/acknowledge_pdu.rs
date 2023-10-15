0c37c60194054fa4ea011367d62c7912331224a8
Cameron Howell <crhowell3@gmail.com>
2023-10-15 00:48:31 +0000

    Acknowledge Pdu crate finished

--- src/dis6/simulation_management/acknowledge_pdu.rs
+++ src/dis6/simulation_management/acknowledge_pdu.rs
@@ -1,7 +1,11 @@
+use bytes::{Buf, BufMut, BytesMut};
 
-use bytes::{BytesMut, BufMut, Buf};
-
-use crate::dis6::common::{pdu_header::{PduHeader, PduType, ProtocolFamily}, entity_id::EntityId, pdu::Pdu, dis_error::DISError};
+use crate::dis6::common::{
+    dis_error::DISError,
+    entity_id::EntityId,
+    pdu::Pdu,
+    pdu_header::{PduHeader, PduType, ProtocolFamily},
+};
 
 #[derive(Copy, Clone, Debug)]
 pub struct AcknowledgePdu {
@@ -16,11 +20,15 @@ pub struct AcknowledgePdu {
 impl AcknowledgePdu {
     pub fn default() -> Self {
         AcknowledgePdu {
-            pdu_header: PduHeader::default(PduType::Acknowledge, ProtocolFamily::SimulationManagement, 56),
+            pdu_header: PduHeader::default(
+                PduType::Acknowledge,
+                ProtocolFamily::SimulationManagement,
+                56,
+            ),
             originating_entity_id: EntityId::default(1),
             receiving_entity_id: EntityId::default(2),
-            acknowledge_flag: ,
-            response_flag: ,
+            acknowledge_flag: AcknowledgeFlag::CreateEntity,
+            response_flag: ResponseFlag::Other,
             request_id: 0,
         }
     }
@@ -36,7 +44,10 @@ impl Pdu for AcknowledgePdu {
         buf.put_u32(self.request_id as u32);
     }
 
-    fn deserialize(mut buffer: BytesMut) -> Result<Self, crate::dis6::common::dis_error::DISError> where Self: Sized {
+    fn deserialize(mut buffer: BytesMut) -> Result<Self, crate::dis6::common::dis_error::DISError>
+    where
+        Self: Sized,
+    {
         let pdu_header = PduHeader::decode(&mut buffer);
         if pdu_header.pdu_type == PduType::Acknowledge {
             let originating_entity_id = EntityId::decode(&mut buffer);
@@ -52,7 +63,7 @@ impl Pdu for AcknowledgePdu {
                 acknowledge_flag,
                 response_flag,
                 request_id,
-            })
+            });
         } else {
             Err(DISError::InvalidDISHeader)
         }
@@ -61,22 +72,41 @@ impl Pdu for AcknowledgePdu {
 
 #[derive(Copy, Clone, Debug)]
 pub enum AcknowledgeFlag {
-    
+    CreateEntity = 1,
+    RemoveEntity = 2,
+    StartResume = 3,
+    StopFreeze = 4,
+    TransferOwnership = 5,
 }
 
 impl AcknowledgeFlag {
     pub fn from_u8(bit: u8) -> AcknowledgeFlag {
-        
+        match bit {
+            1 => AcknowledgeFlag::CreateEntity,
+            2 => AcknowledgeFlag::RemoveEntity,
+            3 => AcknowledgeFlag::StartResume,
+            4 => AcknowledgeFlag::StopFreeze,
+            5 => AcknowledgeFlag::TransferOwnership,
+        }
     }
 }
 
 #[derive(Copy, Clone, Debug)]
 pub enum ResponseFlag {
-    
+    Other = 0,
+    AbleToComply = 1,
+    UnableToComply = 2,
+    PendingOperatorAction = 3,
 }
 
 impl ResponseFlag {
     pub fn from_u8(bit: u8) -> ResponseFlag {
-        
+        match bit {
+            0 => ResponseFlag::Other,
+            1 => ResponseFlag::AbleToComply,
+            2 => ResponseFlag::UnableToComply,
+            3 => ResponseFlag::PendingOperatorAction,
+            4_u8..=u8::MAX => ResponseFlag::Other,
+        }
     }
 }
