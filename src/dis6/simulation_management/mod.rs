2971e490dd1bfe182ae8807ec09efe62c8b3a389
Cameron Howell <crhowell3@gmail.com>
2023-10-16 10:24:23 -0500

    Populated mod file for simulation management family

--- src/dis6/simulation_management/mod.rs
+++ src/dis6/simulation_management/mod.rs
@@ -1,4 +1,32 @@
+//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation
+//                     (DIS) application protocol v6 and v7
+//     Copyright (C) 2023 Cameron Howell
+//
+//     Licensed under the BSD 2-Clause License
+
 pub mod acknowledge_pdu;
 pub mod acknowledge_reliable_pdu;
+pub mod action_request_pdu;
+pub mod action_request_reliable_pdu;
+pub mod action_response_pdu;
+pub mod action_response_reliable_pdu;
+pub mod comment_pdu;
+pub mod comment_reliable_pdu;
+pub mod create_entity_pdu;
+pub mod create_entity_reliable_pdu;
 pub mod data_pdu;
 pub mod data_query_pdu;
+pub mod data_query_reliable_pdu;
+pub mod data_reliable_pdu;
+pub mod event_report_pdu;
+pub mod event_report_reliable_pdu;
+pub mod record_query_reliable_pdu;
+pub mod remove_entity_pdu;
+pub mod remove_entity_reliable_pdu;
+pub mod set_data_pdu;
+pub mod set_data_reliable_pdu;
+pub mod set_record_reliable_pdu;
+pub mod start_resume_pdu;
+pub mod start_resume_reliable_pdu;
+pub mod stop_freeze_pdu;
+pub mod stop_freeze_reliable_pdu;
