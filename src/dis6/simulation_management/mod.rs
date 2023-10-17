f712afe987b886e1eedd94d217df9b71324b19f5
Cameron Howell <crhowell3@gmail.com>
2023-10-17 23:03:48 +0000

    Moved reliability pdus to their own family dir

--- src/dis6/simulation_management/mod.rs
+++ src/dis6/simulation_management/mod.rs
@@ -5,28 +5,14 @@
 //     Licensed under the BSD 2-Clause License
 
 pub mod acknowledge_pdu;
-pub mod acknowledge_reliable_pdu;
 pub mod action_request_pdu;
-pub mod action_request_reliable_pdu;
 pub mod action_response_pdu;
-pub mod action_response_reliable_pdu;
 pub mod comment_pdu;
-pub mod comment_reliable_pdu;
 pub mod create_entity_pdu;
-pub mod create_entity_reliable_pdu;
 pub mod data_pdu;
 pub mod data_query_pdu;
-pub mod data_query_reliable_pdu;
-pub mod data_reliable_pdu;
 pub mod event_report_pdu;
-pub mod event_report_reliable_pdu;
-pub mod record_query_reliable_pdu;
 pub mod remove_entity_pdu;
-pub mod remove_entity_reliable_pdu;
 pub mod set_data_pdu;
-pub mod set_data_reliable_pdu;
-pub mod set_record_reliable_pdu;
 pub mod start_resume_pdu;
-pub mod start_resume_reliable_pdu;
 pub mod stop_freeze_pdu;
-pub mod stop_freeze_reliable_pdu;
