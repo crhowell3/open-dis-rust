9192c2e7cc87e01a59db5a736e513b111b1a8ec8
Cameron Howell <crhowell3@gmail.com>
2023-11-03 03:49:57 +0000

    Progress on EE beam and system data

--- src/distributed_emissions/electromagnetic_emission_beam_data.rs
+++ src/distributed_emissions/electromagnetic_emission_beam_data.rs
@@ -1,6 +1,10 @@
 use bytes::{Buf, BufMut, BytesMut};
 
-#[derive(Copy, Clone, Debug)]
+use super::{
+    fundamental_parameter_data::FundamentalParameterData, track_jam_target::TrackJamTarget,
+};
+
+#[derive(Clone, Debug)]
 pub struct ElectromagneticEmissionBeamData {
     pub beam_data_length: u8,
     pub beam_id_number: u8,
@@ -14,4 +18,63 @@ pub struct ElectromagneticEmissionBeamData {
     pub track_jam_targets: Vec<TrackJamTarget>,
 }
 
-impl ElectromagneticEmissionBeamData {}
+impl ElectromagneticEmissionBeamData {
+    pub fn default() -> Self {
+        ElectromagneticEmissionBeamData {
+            beam_data_length: 0,
+            beam_id_number: 0,
+            beam_parameter_index: 0,
+            fundamental_parameter_data: FundamentalParameterData::default(),
+            beam_function: 0,
+            number_of_track_jam_targets: 0,
+            high_density_track_jam: 0,
+            pad4: 0,
+            jamming_mode_sequence: 0,
+            track_jam_targets: vec![],
+        }
+    }
+
+    pub fn serialize(&self, buf: &mut BytesMut) {
+        buf.put_u8(self.beam_data_length as u8);
+        buf.put_u8(self.beam_id_number as u8);
+        buf.put_u8(self.beam_parameter_index as u8);
+        self.fundamental_parameter_data.serialize(buf);
+        buf.put_u8(self.beam_function as u8);
+        buf.put_u8(self.number_of_track_jam_targets as u8);
+        buf.put_u8(self.high_density_track_jam as u8);
+        buf.put_u8(self.pad4 as u8);
+        buf.put_u32(self.jamming_mode_sequence as u32);
+        for target in self.track_jam_targets {
+            target.serialize(buf);
+        }
+    }
+
+    pub fn decode(buf: &mut BytesMut) -> ElectromagneticEmissionBeamData {
+        let beam_data_length = buf.get_u8();
+        let beam_id_number = buf.get_u8();
+        let beam_parameter_index = buf.get_u8();
+        let fundamental_parameter_data = FundamentalParameterData::decode(buf);
+        let beam_function = buf.get_u8();
+        let number_of_track_jam_targets = buf.get_u8();
+        let high_density_track_jam = buf.get_u8();
+        let pad4 = buf.get_u8();
+        let jamming_mode_sequence = buf.get_u32();
+        let mut track_jam_targets: Vec<TrackJamTarget> = vec![];
+        for i in 0..number_of_track_jam_targets {
+            track_jam_targets.push(TrackJamTarget::decode(buf));
+        }
+
+        ElectromagneticEmissionBeamData {
+            beam_data_length,
+            beam_id_number,
+            beam_parameter_index,
+            fundamental_parameter_data,
+            beam_function,
+            number_of_track_jam_targets,
+            high_density_track_jam,
+            pad4,
+            jamming_mode_sequence,
+            track_jam_targets,
+        }
+    }
+}
--- src/distributed_emissions/electromagnetic_emission_system_data.rs
+++ src/distributed_emissions/electromagnetic_emission_system_data.rs
@@ -1,4 +1,10 @@
-#[derive(Copy, Clone, Debug)]
+use super::electromagnetic_emission_beam_data::ElectromagneticEmissionBeamData;
+use bytes::{Buf, BufMut, BytesMut};
+
+use crate::common::vector3_float::Vector3Float;
+use crate::distributed_emissions::emitter_system::EmitterSystem;
+
+#[derive(Clone, Debug)]
 pub struct ElectromagneticEmissionSystemData {
     pub system_data_length: u8,
     pub number_of_beams: u8,
@@ -6,4 +12,49 @@ pub struct ElectromagneticEmissionSystemData {
     pub emitter_system: EmitterSystem,
     pub location: Vector3Float,
     pub beam_data_records: Vec<ElectromagneticEmissionBeamData>,
+}
+
+impl ElectromagneticEmissionSystemData {
+    pub fn default() -> Self {
+        ElectromagneticEmissionSystemData {
+            system_data_length: 0,
+            number_of_beams: 0,
+            emissions_padding2: 0,
+            emitter_system: EmitterSystem::default(),
+            location: Vector3Float::new(0.0, 0.0, 0.0),
+            beam_data_records: vec![],
+        }
+    }
+
+    pub fn serialize(&self, buf: &mut BytesMut) {
+        buf.put_u8(self.system_data_length as u8);
+        buf.put_u8(self.number_of_beams as u8);
+        buf.put_u8(self.emissions_padding2 as u8);
+        self.emitter_system.serialize(buf);
+        self.location.serialize(buf);
+        for beams in self.beam_data_records {
+            beams.serialize(buf);
+        }
+    }
+
+    pub fn decode(buf: &mut BytesMut) -> ElectromagneticEmissionSystemData {
+        let system_data_length = buf.get_u8();
+        let number_of_beams = buf.get_u8();
+        let emissions_padding2 = buf.get_u8();
+        let emitter_system = EmitterSystem::decode(buf);
+        let location = Vector3Float::decode(buf);
+        let mut beam_data_records: Vec<ElectromagneticEmissionBeamData> = vec![];
+        for i in 0..number_of_beams {
+            beam_data_records.push(ElectromagneticEmissionBeamData::decode(buf));
+        }
+
+        ElectromagneticEmissionSystemData {
+            system_data_length,
+            number_of_beams,
+            emissions_padding2,
+            emitter_system,
+            location,
+            beam_data_records,
+        }
+    }
 }
