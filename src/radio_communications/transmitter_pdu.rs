4e845e38afdecf67e1ab4a0d85842b1a96d18d17
Cameron <54868046+crhowell3@users.noreply.github.com>
2023-12-18 10:04:14 -0600

    Whoops

--- src/radio_communications/transmitter_pdu.rs
+++ src/radio_communications/transmitter_pdu.rs
@@ -60,7 +60,7 @@ impl Default for TransmitterPdu {
             pdu_header: PduHeader::default(
                 PduType::Transmitter,
                 ProtocolFamily::RadioCommunications,
-                56,
+                86,
             ),
             entity_id: EntityId::default(1),
             radio_id: 0,
