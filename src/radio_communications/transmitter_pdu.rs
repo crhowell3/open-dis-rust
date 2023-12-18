bc4d604700f47e579bab86cef15a41262348c277
Cameron <54868046+crhowell3@users.noreply.github.com>
2023-12-18 10:11:45 -0600

    Temp

--- src/radio_communications/transmitter_pdu.rs
+++ src/radio_communications/transmitter_pdu.rs
@@ -60,7 +60,7 @@ impl Default for TransmitterPdu {
             pdu_header: PduHeader::default(
                 PduType::Transmitter,
                 ProtocolFamily::RadioCommunications,
-                86,
+                128,
             ),
             entity_id: EntityId::default(1),
             radio_id: 0,
@@ -236,7 +236,7 @@ mod tests {
         let pdu_header = PduHeader::default(
             PduType::Transmitter,
             ProtocolFamily::RadioCommunications,
-            896 / 8,
+            1024 / 8,
         );
 
         assert_eq!(
