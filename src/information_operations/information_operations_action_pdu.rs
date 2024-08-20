ea7af8f80b32e843f4bd642ae1fdf378fad1a453
Cameron Howell <jango.djarin@proton.me>
2024-08-19 22:12:07 -0500

    Finished IO Action PDU

--- src/information_operations/information_operations_action_pdu.rs
+++ src/information_operations/information_operations_action_pdu.rs
@@ -21,7 +21,7 @@ use crate::{
     warfare::data_types::standard_variable_specification::StandardVariableSpecification,
 };
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Debug)]
 /// Implemented according to IEEE 1278.1-2012
 pub struct InformationOperationsActionPdu {
     pub pdu_header: PduHeader,
@@ -36,8 +36,7 @@ pub struct InformationOperationsActionPdu {
     pub io_attacker_entity_id: EntityId,
     pub io_primary_target_entity_id: EntityId,
     pub padding2: u16,
-    pub number_of_io_records: u16,
-    pub io_records: Vec<StandardVariableSpecification>,
+    pub io_records: StandardVariableSpecification,
 }
 
 impl Default for InformationOperationsActionPdu {
@@ -58,6 +57,18 @@ impl Default for InformationOperationsActionPdu {
                 ProtocolFamily::InformationOperations,
                 32,
             ),
+            originating_simulation_id: EntityId::default(1),
+            receiving_simulation_id: EntityId::default(2),
+            request_id: 0,
+            io_warfare_type: IOActionIOWarfareType::default(),
+            io_simulation_source: IOActionIOSimulationSource::default(),
+            io_action_type: IOActionIOActionType::default(),
+            io_action_phase: IOActionIOActionPhase::default(),
+            padding1: 0,
+            io_attacker_entity_id: EntityId::default(3),
+            io_primary_target_entity_id: EntityId::default(4),
+            padding2: 0,
+            io_records: StandardVariableSpecification::default(),
         }
     }
 }
@@ -67,6 +78,18 @@ impl Pdu for InformationOperationsActionPdu {
         self.pdu_header.length = u16::try_from(std::mem::size_of_val(self))
             .expect("The length of the PDU should fit in a u16.");
         self.pdu_header.serialize(buf);
+        self.originating_simulation_id.serialize(buf);
+        self.receiving_simulation_id.serialize(buf);
+        buf.put_u32(self.request_id);
+        buf.put_u8(self.io_warfare_type as u8);
+        buf.put_u8(self.io_simulation_source as u8);
+        buf.put_u8(self.io_action_type as u8);
+        buf.put_u8(self.io_action_phase as u8);
+        buf.put_u32(self.padding1);
+        self.io_attacker_entity_id.serialize(buf);
+        self.io_primary_target_entity_id.serialize(buf);
+        buf.put_u16(self.padding2);
+        self.io_records.serialize(buf);
     }
 
     fn deserialize(mut buffer: BytesMut) -> Result<Self, DISError>
@@ -75,7 +98,33 @@ impl Pdu for InformationOperationsActionPdu {
     {
         let pdu_header = PduHeader::decode(&mut buffer);
         if pdu_header.pdu_type == PduType::InformationOperationsAction {
-            Ok(InformationOperationsActionPdu { pdu_header })
+            let originating_simulation_id = EntityId::decode(&mut buffer);
+            let receiving_simulation_id = EntityId::decode(&mut buffer);
+            let request_id = buffer.get_u32();
+            let io_warfare_type = IOActionIOWarfareType::decode(&mut buffer);
+            let io_simulation_source = IOActionIOSimulationSource::decode(&mut buffer);
+            let io_action_type = IOActionIOActionType::decode(&mut buffer);
+            let io_action_phase = IOActionIOActionPhase::decode(&mut buffer);
+            let padding1 = buffer.get_u32();
+            let io_attacker_entity_id = EntityId::decode(&mut buffer);
+            let io_primary_target_entity_id = EntityId::decode(&mut buffer);
+            let padding2 = buffer.get_u16();
+            let io_records = StandardVariableSpecification::decode(&mut buffer);
+            Ok(InformationOperationsActionPdu {
+                pdu_header,
+                originating_simulation_id,
+                receiving_simulation_id,
+                request_id,
+                io_warfare_type,
+                io_simulation_source,
+                io_action_type,
+                io_action_phase,
+                padding1,
+                io_attacker_entity_id,
+                io_primary_target_entity_id,
+                padding2,
+                io_records,
+            })
         } else {
             Err(DISError::InvalidDISHeader)
         }
@@ -92,6 +141,93 @@ impl Pdu for InformationOperationsActionPdu {
     where
         Self: Sized,
     {
-        Ok(InformationOperationsActionPdu { pdu_header })
+        let originating_simulation_id = EntityId::decode(&mut buffer);
+        let receiving_simulation_id = EntityId::decode(&mut buffer);
+        let request_id = buffer.get_u32();
+        let io_warfare_type = IOActionIOWarfareType::decode(&mut buffer);
+        let io_simulation_source = IOActionIOSimulationSource::decode(&mut buffer);
+        let io_action_type = IOActionIOActionType::decode(&mut buffer);
+        let io_action_phase = IOActionIOActionPhase::decode(&mut buffer);
+        let padding1 = buffer.get_u32();
+        let io_attacker_entity_id = EntityId::decode(&mut buffer);
+        let io_primary_target_entity_id = EntityId::decode(&mut buffer);
+        let padding2 = buffer.get_u16();
+        let io_records = StandardVariableSpecification::decode(&mut buffer);
+        Ok(InformationOperationsActionPdu {
+            pdu_header,
+            originating_simulation_id,
+            receiving_simulation_id,
+            request_id,
+            io_warfare_type,
+            io_simulation_source,
+            io_action_type,
+            io_action_phase,
+            padding1,
+            io_attacker_entity_id,
+            io_primary_target_entity_id,
+            padding2,
+            io_records,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::InformationOperationsActionPdu;
+    use crate::common::{
+        pdu::Pdu,
+        pdu_header::{PduHeader, PduType, ProtocolFamily},
+    };
+    use bytes::BytesMut;
+
+    #[test]
+    fn create_header() {
+        let information_operations_action_pdu = InformationOperationsActionPdu::default();
+        let pdu_header = PduHeader::default(
+            PduType::InformationOperationsAction,
+            ProtocolFamily::InformationOperations,
+            32,
+        );
+
+        assert_eq!(
+            pdu_header.protocol_version,
+            information_operations_action_pdu
+                .pdu_header
+                .protocol_version
+        );
+        assert_eq!(
+            pdu_header.exercise_id,
+            information_operations_action_pdu.pdu_header.exercise_id
+        );
+        assert_eq!(
+            pdu_header.pdu_type,
+            information_operations_action_pdu.pdu_header.pdu_type
+        );
+        assert_eq!(
+            pdu_header.protocol_family,
+            information_operations_action_pdu.pdu_header.protocol_family
+        );
+        assert_eq!(
+            pdu_header.length,
+            information_operations_action_pdu.pdu_header.length
+        );
+        assert_eq!(
+            pdu_header.padding,
+            information_operations_action_pdu.pdu_header.padding
+        );
+    }
+
+    #[test]
+    fn deserialize_header() {
+        let mut information_operations_action_pdu = InformationOperationsActionPdu::default();
+        let mut buffer = BytesMut::new();
+        information_operations_action_pdu.serialize(&mut buffer);
+
+        let new_information_operations_action_pdu =
+            InformationOperationsActionPdu::deserialize(buffer).unwrap();
+        assert_eq!(
+            new_information_operations_action_pdu.pdu_header,
+            information_operations_action_pdu.pdu_header
+        );
     }
 }
