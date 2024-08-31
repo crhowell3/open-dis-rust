debf5ea9c24355f6a98baf0c6d5e543a96fd6b81
Cameron Howell <jango.djarin@proton.me>
2024-08-31 00:34:37 -0500

    More docs

--- src/common/clock_time.rs
+++ src/common/clock_time.rs
@@ -1,11 +1,13 @@
-//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation
+//     open-dis-rust - Rust implementation of the IEEE 1278.1-2012 Distributed Interactive
+//                     Simulation (DIS) application protocol
 //     Copyright (C) 2023 Cameron Howell
 //
-//     Licensed under the BSD-2-Clause License
+//     Licensed under the BSD 2-Clause License
 
 use bytes::{Buf, BufMut, BytesMut};
 
 #[derive(Copy, Clone, Debug, Default)]
+/// Implemented according to IEEE 1278.1-2012 §6.2.14
 pub struct ClockTime {
     pub hour: u32,
     pub time_past_hour: u32,
--- src/common/dis_error.rs
+++ src/common/dis_error.rs
@@ -1,9 +1,11 @@
-//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation
+//     open-dis-rust - Rust implementation of the IEEE 1278.1-2012 Distributed Interactive
+//                     Simulation (DIS) application protocol
 //     Copyright (C) 2023 Cameron Howell
 //
-//     Licensed under the BSD-2-Clause License
+//     Licensed under the BSD 2-Clause License
 
 #[derive(Debug)]
+/// Enumeration for describing types of errors that may occur related to PDUs
 pub enum DISError {
     InvalidDISHeader,
 }
