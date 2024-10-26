d1c06cad52cc85390c4088020b72e5f8ffe35a50
Cameron Howell <jango.djarin@proton.me>
2024-10-25 21:26:30 -0500

    Added headers and struct documentation strings for radio comm and siman pdus

--- src/simulation_management/start_resume_pdu.rs
+++ src/simulation_management/start_resume_pdu.rs
@@ -1,3 +1,9 @@
+//     open-dis-rust - Rust implementation of the IEEE 1278.1-2012 Distributed Interactive
+//                     Simulation (DIS) application protocol
+//     Copyright (C) 2023 Cameron Howell
+//
+//     Licensed under the BSD 2-Clause License
+
 use bytes::{Buf, BufMut, BytesMut};
 use std::any::Any;
 
@@ -10,6 +16,7 @@ use crate::common::{
 };
 
 #[derive(Copy, Clone, Debug)]
+/// Implemented according to IEEE 1278.1-2012 §7.5.4
 pub struct StartResumePdu {
     pub pdu_header: PduHeader,
     pub originating_entity_id: EntityId,
