12a08817e9650dc9e45439247d5ca30f75ae96d7
crhowell3 <crhowell3@users.noreply.github.com>
2024-11-23 06:36:18 +0000

    deploy: 24567695c9b1a55927e04147dfece85ecf64a1dc

--- src/information_operations/mod.rs
+++
@@ -1,8 +0,0 @@
-//     open-dis-rust - Rust implementation of the IEEE 1278.1-2012 Distributed Interactive
-//                     Simulation (DIS) application protocol
-//     Copyright (C) 2023 Cameron Howell
-//
-//     Licensed under the BSD 2-Clause License
-
-pub mod information_operations_action_pdu;
-pub mod information_operations_report_pdu;
--- src/radio_communications/data_types/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod intercom_communications_parameters;
-pub mod modulation_type;
-pub mod radio_entity_type;
