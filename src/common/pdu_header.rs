3ff0b2efa22c4fe7ac2830a780edcb0c79485922
Cameron <jango.djarin@proton.me>
2025-11-25 18:21:23 -0600

    Cleaned up redundant code in pdu_header mod (#76)

--- src/common/pdu_header.rs
+++ src/common/pdu_header.rs
@@ -155,7 +155,7 @@ impl Default for PduHeader {
             pdu_type: PduType::default(),
             protocol_family: ProtocolFamily::default(),
             timestamp: PduHeader::calculate_dis_timestamp(),
-            length: 0_u16,
+            length: 0,
             status_record: PduStatusRecord::default(),
         }
     }
@@ -205,127 +205,15 @@ impl PduHeader {
         buf.put_u8(self.status_record.to_u8());
     }
 
-    fn deserialize_protocol_version(data: u8) -> ProtocolVersion {
-        match data {
-            1 => ProtocolVersion::DIS_PDUv1,
-            2 => ProtocolVersion::IEEE1278_1993,
-            3 => ProtocolVersion::DIS_PDUv2_Third_Draft,
-            4 => ProtocolVersion::DIS_PDUv2_Fourth_Draft_Revised,
-            5 => ProtocolVersion::IEEE1278_1_1995,
-            6 => ProtocolVersion::IEEE1278_1A_1998,
-            7 => ProtocolVersion::IEEE1278_1_2012,
-            _ => ProtocolVersion::Other,
-        }
-    }
-
     pub fn deserialize<B: Buf>(buf: &mut B) -> PduHeader {
         PduHeader {
-            protocol_version: PduHeader::deserialize_protocol_version(buf.get_u8()),
+            protocol_version: ProtocolVersion::deserialize(buf),
             exercise_id: buf.get_u8(),
-            pdu_type: PduHeader::deserialize_pdu_type(buf.get_u8()),
-            protocol_family: PduHeader::deserialize_protocol_family(buf.get_u8()),
+            pdu_type: PduType::deserialize(buf),
+            protocol_family: ProtocolFamily::deserialize(buf),
             timestamp: buf.get_u32(),
             length: buf.get_u16(),
             status_record: PduStatusRecord::from_u8(buf.get_u8()),
-        }
-    }
-
-    #[must_use]
-    pub fn deserialize_pdu_type(data: u8) -> PduType {
-        match data {
-            1 => PduType::EntityState,
-            2 => PduType::Fire,
-            3 => PduType::Detonation,
-            4 => PduType::Collision,
-            5 => PduType::ServiceRequest,
-            6 => PduType::ResupplyOffer,
-            7 => PduType::ResupplyReceived,
-            8 => PduType::ResupplyCancel,
-            9 => PduType::RepairComplete,
-            10 => PduType::RepairResponse,
-            11 => PduType::CreateEntity,
-            12 => PduType::RemoveEntity,
-            13 => PduType::StartResume,
-            14 => PduType::StopFreeze,
-            15 => PduType::Acknowledge,
-            16 => PduType::ActionRequest,
-            17 => PduType::ActionResponse,
-            18 => PduType::DataQuery,
-            19 => PduType::SetData,
-            20 => PduType::Data,
-            21 => PduType::EventReport,
-            22 => PduType::Comment,
-            23 => PduType::ElectromagneticEmission,
-            24 => PduType::Designator,
-            25 => PduType::Transmitter,
-            26 => PduType::Signal,
-            27 => PduType::Receiver,
-            28 => PduType::IFF,
-            29 => PduType::UnderwaterAcoustic,
-            30 => PduType::SupplementalEmission,
-            31 => PduType::IntercomSignal,
-            32 => PduType::IntercomControl,
-            33 => PduType::AggregateState,
-            34 => PduType::IsGroupOf,
-            35 => PduType::TransferOwnership,
-            36 => PduType::IsPartOf,
-            37 => PduType::MinefieldState,
-            38 => PduType::MinefieldQuery,
-            39 => PduType::MinefieldData,
-            40 => PduType::MinefieldResponseNack,
-            41 => PduType::EnvironmentalProcess,
-            42 => PduType::GriddedData,
-            43 => PduType::PointObjectState,
-            44 => PduType::LinearObjectState,
-            45 => PduType::ArealObjectState,
-            46 => PduType::TSPI,
-            47 => PduType::Appearance,
-            48 => PduType::ArticulatedParts,
-            49 => PduType::LEFire,
-            50 => PduType::LEDetonation,
-            51 => PduType::CreateEntityReliable,
-            52 => PduType::RemoveEntityReliable,
-            53 => PduType::StartResumeReliable,
-            54 => PduType::StopFreezeReliable,
-            55 => PduType::AcknowledgeReliable,
-            56 => PduType::ActionRequestReliable,
-            57 => PduType::ActionResponseReliable,
-            58 => PduType::DataQueryReliable,
-            59 => PduType::SetDataReliable,
-            60 => PduType::DataReliable,
-            61 => PduType::EventReportReliable,
-            62 => PduType::CommentReliable,
-            63 => PduType::RecordReliable,
-            64 => PduType::SetRecordReliable,
-            65 => PduType::RecordQueryReliable,
-            66 => PduType::CollisionElastic,
-            67 => PduType::EntityStateUpdate,
-            68 => PduType::DirectedEnergyFire,
-            69 => PduType::EntityDamageStatus,
-            70 => PduType::InformationOperationsAction,
-            71 => PduType::InformationOperationsReport,
-            72 => PduType::Attribute,
-            _ => PduType::Other,
-        }
-    }
-
-    #[must_use]
-    fn deserialize_protocol_family(data: u8) -> ProtocolFamily {
-        match data {
-            1 => ProtocolFamily::EntityInformation,
-            2 => ProtocolFamily::Warfare,
-            3 => ProtocolFamily::Logistics,
-            4 => ProtocolFamily::RadioCommunications,
-            5 => ProtocolFamily::SimulationManagement,
-            6 => ProtocolFamily::DistributedEmissionRegeneration,
-            7 => ProtocolFamily::EntityManagement,
-            8 => ProtocolFamily::Minefield,
-            9 => ProtocolFamily::SyntheticEnvironment,
-            10 => ProtocolFamily::SimulationManagementWithReliability,
-            11 => ProtocolFamily::LiveEntityInformationInteraction,
-            12 => ProtocolFamily::NonRealTime,
-            13 => ProtocolFamily::InformationOperations,
-            _ => ProtocolFamily::Other,
         }
     }
 }
