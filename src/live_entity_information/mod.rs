910b95dca740febb3e7dcb555822b6937f47ed5e
Cameron Howell <jango.djarin@proton.me>
2025-11-25 20:05:29 -0600

    Live entity protocol start

---
+++ src/live_entity_information/mod.rs
@@ -0,0 +1,12 @@
+//     open-dis-rust - Rust implementation of the IEEE-1278.1 Distributed Interactive Simulation
+//     Copyright (C) 2025 Cameron Howell
+//
+//     Licensed under the BSD-2-Clause License
+
+//! The Live Entity (LE) Information/Interaction protocol family
+
+pub mod appearance_pdu;
+pub mod articulated_parts_pdu;
+pub mod live_entity_detonation_pdu;
+pub mod live_entity_fire_pdu;
+pub mod time_space_position_information_pdu;
